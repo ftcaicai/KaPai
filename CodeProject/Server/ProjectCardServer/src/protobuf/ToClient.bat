aef72ade49f92f53231efbeb03aeda233816bc42
FtLover <ftcaicai@gmail.com>
2014-01-05 18:04:57 +0800

    添加protobuf转换批处理

---
+++ CodeProject/Server/ProjectCardServer/src/protobuf/ToClient.bat
@@ -0,0 +1,12 @@
+subst m: /d
+subst m: "%~dp0..\..\..\..\..\CodeProject\Client\UnityProject"
+
+@SET PROTOBUF="%~dp0..\..\..\..\..\CodeProject\Tools\protobuf\protogen.exe"
+
+@%PROTOBUF% -i:ExternalCommonProtocol.proto -o:myth.cs
+
+@xcopy myth.cs m:\Assets\Scripts\	/Y /Q
+@del myth.cs
+
+subst m: /d
+@pause
